3d9696df4cdff9ddb84c7c9626ea77f172de458b
Jordan Lee <jbjordanleemac@gmail.com>
2020-09-22 14:05:46 -0700

    update merge file 205

--- merge_lists_092220.py
+++ merge_lists_092220.py
@@ -1,5 +1,20 @@
 #!/usr/bin/python
 
+class total():
+  def __init__(self, g, h):
+    self.g = g
+    self.h = h
+
+  def reversethem(self):
+    print('After reverse two numbers becomes ' + str(self.h) + ' then ' + str(self.g) + ' done ')
+
+# prompt user for nubmer
+
+d = raw_input('Enter first number: ')
+e = raw_input('Enter second number: ')
+t1 = total(d, e)
+t1.reversethem()
+
 a = ['a', 'b', 'c']
 b = ['d', 'e', 'f']
 
@@ -9,3 +24,7 @@ for c in b:
   a.append(c)
 
 print(a)
+
+
+
+
